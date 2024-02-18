867e321afdbc11dd88ce475c83ee842afb044676
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2024-02-18 11:06:55 +0530

    This commit is been made before pulling the previous data from the team-mate.

---
+++ test7.js
@@ -0,0 +1,3 @@
+console.log("Here you were unaware about a chnage and doing your work later you added this changes.");
+console.log("And commited these changes now want to push it then it will throw an error at the of push.");
+console.log("This is because before you do your push you need to pull all the changes made in the repo at that interval of time into your local remote repo.");
