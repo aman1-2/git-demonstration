20e22628b3c32e6660a2e4750028c008d9b00731
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2024-02-18 09:55:25 +0530

    A new commit has been made in the new_branch to show case the separation concern.

---
+++ test6.js
@@ -0,0 +1,4 @@
+console.log("This file is created after making a new branch, Then we moved back to the master branch.");
+console.log("Made some commit in the master branch after the creation of the new branch.");
+console.log("To demonstrate that new commit inside saved in the new_branch it is segregated from the master branch.");
+console.log("Now we are on our new_branch and going to do a commit here and this commit wont't be showed in the master branch. Untill and unless we merge both the branches.")
